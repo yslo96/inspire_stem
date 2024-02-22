7d8960e98702ec53c87f4db448956db42ccde973
mwaririlawrence96@gmail.com <mwaririlaawrence96@gmail.com>
2024-02-22 10:15:03 +0300

    loops,strings

---
+++ week_2/loops.py
@@ -0,0 +1,16 @@
+# loops and iterations
+#Date: 22/02/2024
+#Name: Lawrence Mwariri
+
+# for loop
+# while loop
+# 
+
+for number in range(0,10):
+    print(number)
+
+#squares of the numbers
+    
+for x in range(0,11):
+    print(str(x**2) + "\t",end=" ")
+     
---
+++ week_2/strings.py
@@ -0,0 +1,50 @@
+# Strings in python
+#Date: 22/02/2024
+# Name: Lawrence Mwariri
+
+city = "nairobi"
+
+#convert to uppercase
+
+print(city[-1])
+print(city[-2])
+print(city[-3])
+print(city[-4])
+print(city[-5])
+print(city[-6])
+print(city[-7])
+
+print("\n")# prints a new line
+print(city.upper())
+name = "LAWRENCE MWARIRI"
+
+print(name)
+print(name.lower())# converts string to lower case
+
+town = "  Naivasha  "
+
+print(town)
+print("\t")# new tab
+print(town.strip())
+
+f_name = "Lawrence"
+s_name = "Mwariri"
+
+full_name = f_name + s_name
+
+print(full_name)
+
+#replacing a character
+
+fruit = "Apple"
+
+print(fruit.replace("A","Y"))
+
+subject = "Physical,Science"
+
+print(subject.split(","))
+
+age = 17
+height = 1.4
+
+print("I am {0} years old and {1} metres tall " .format(age,height))
