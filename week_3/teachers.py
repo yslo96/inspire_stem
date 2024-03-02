317343e0576eee95ce444520709c3e30c276fb8d
yslo96 <159754891+yslo96@users.noreply.github.com>
2024-03-02 12:12:30 +0300

    Add files via upload

---
+++ week_3/teachers.py
@@ -0,0 +1,8 @@
+def print_name(name):
+    print("My name is {0}".format(name))
+
+def print_age(age):
+    print("My age is {0}".format(age))
+
+def print_course(name,course):
+    print("Prof {0} teaches {1} at JKUAT" .format(name,course))
