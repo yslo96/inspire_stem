9d8110f994ab42e55a59c5d7f70005b62e5dfcdb
mwaririlawrence96@gmail.com <mwaririlaawrence96@gmail.com>
2024-03-21 08:24:13 +0300

    calc,guess_game

--- week_5/web/guess_game/guess.js
+++ week_5/web/guess_game/guess.js
@@ -1,1 +1,23 @@
-alert("Welcome") 
+const randomNumber = Math.floor(Math.random() * 100)
+console.log(randomNumber)
+let attempt = 1;
+
+function checkGuess(){
+    const guess = parseInt(document.getElementById("guessField").value)
+    attempt++;
+    if(isNaN(guess) || guess < 1 || guess > 100){
+        setMessage("Please a valid number between 1 and 100")
+        return;
+    }
+    if(guess === randomNumber){
+        setMessage("NIGGA umeomoka")
+        document.getElementById("guessField").disable = true;
+    }else if(guess < randomNumber){
+        setMessage("Changamka bana")
+    }else(
+        setMessage("Punguza pace")
+    )
+}
+function setMessage(message){
+    document.getElementById("message").textContent = message
+}
